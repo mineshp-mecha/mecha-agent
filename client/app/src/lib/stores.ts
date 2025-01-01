f2798f47de3e4f60f24faa6791c694f4bd256f39
swetar-mecha <swetar@mechasystems.com>
2025-01-01 17:38:18 +0530

    fix: machine info store

--- client/app/src/lib/stores.ts
+++ client/app/src/lib/stores.ts
@@ -1,6 +1,8 @@
-import { writable } from 'svelte/store';
+import { readonly, writable } from 'svelte/store';
 import type { MachineDataType } from '../interfaces';
 
 export let machineInfo = writable<MachineDataType>({} as MachineDataType);
+export let readableMachineInfo = readonly(machineInfo);
+
 
 export const count = writable(0);
